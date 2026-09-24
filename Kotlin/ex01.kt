24c36666483205ff244dbcf1a40f17c6d48a982b
Matheus Scomacao 1DS <matheus.campregher@escola.pr.gov.br>
2026-09-24 11:33:49 -0300

    Ex01

---
+++ Kotlin/ex01.kt
@@ -0,0 +1,8 @@
+fun main() {
+  println("=========================")
+  println("MEU PRIMEIRO PROGRAMA")
+  println("=========================")
+  println("Aluno: Matheus Scomacao")
+  println("Turma: 1° DS")
+  println("Escola: Alberto Gomes Veiga")
+  println("Bem Vindo ao Kotlin")
